6322ee51881552ed83bdc8393d9f371d18c9d093
mihailkostromin <kostrominmi@mail.ru>
2023-11-08 23:20:39 +0300

    add log and AtomicInteger. Проверяется атомарно

--- src/Fork.java
+++ src/Fork.java
@@ -1,14 +1,33 @@
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Fork {
-    private Lock lock = new ReentrantLock();
+    //    private Lock lock = new ReentrantLock();
+//
+//    public void pickUp() {
+//        lock.lock();
+//    }
+//
+//    public void putDown() {
+//        lock.unlock();
+//    }
+    private AtomicInteger ownerNumber; // номер философа. Проверяется и изменяется философами атомарно,
+    // что исключает возможность взятия вилки двумя философами
+    private static int count;
+    private int index;
 
-    public void pickUp() {
-        lock.lock();
+    public Fork() {
+        index = ++count;
+        ownerNumber = new AtomicInteger(0);
     }
 
-    public void putDown() {
-        lock.unlock();
+    public AtomicInteger getOwnerNumber() {
+        return ownerNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Fork " + index;
     }
 }
--- src/Philosopher.java
+++ src/Philosopher.java
@@ -1,48 +1,85 @@
+
 public class Philosopher extends Thread {
     private String name;
     private Fork leftFork;
     private Fork rightFork;
     private PhilosopherState state = PhilosopherState.THINKING;
-    private int eatCount = 0;
+    private int eatCount = 0; // уровень сытости
+    private int number;
+    private static int count;
+    private StringBuilder log;
 
     public Philosopher(String name, Fork leftFork, Fork rightFork) {
         this.name = name;
         this.leftFork = leftFork;
         this.rightFork = rightFork;
+        number = ++count;
+        log = new StringBuilder();
     }
 
     @Override
     public void run() {
         try {
             while (eatCount < 3) {
-                think();
-                eat();
+                // проверяем состояние атомарного поля левой вилки. если равно 0, то устаналиваем значение, равное number философа
+                if (leftFork.getOwnerNumber().compareAndSet(0, number)) {
+
+                    // аналогично проверяем состояние правой вилки
+                    if (rightFork.getOwnerNumber().compareAndSet(0, number)) {
+                        // если обе вилки были свободны, то выводим сообщение, что философ взял их в руку и ест 20 мс
+                        System.out.println(">>> " + name + " взял " + leftFork + " и " + rightFork + ", ЕСТ " + ++eatCount + " РАЗ \t\t");
+                        log.append("* ПОЕЛ");
+                        sleep(20);
+
+                        // после того, как философ поел, он кладет вилки на стол и устанавливает значение атомарного поля = 0
+                        System.out.println("<<< " + name + " кладет " + leftFork + " и " + rightFork);
+                        rightFork.getOwnerNumber().compareAndSet(number, 0);
+                        leftFork.getOwnerNumber().compareAndSet(number, 0);
+
+                        // ждет еще 20 мс, чтобы другие философы успели взять освободившиеся вилки
+                        sleep(20);
+
+                    } else {
+                        // если правая вилка занята, атомарное значение левой вилки устанавливается равным 0 и философ думает 18 мс
+                        leftFork.getOwnerNumber().compareAndSet(number, 0);
+                        think();
+                    }
+                } else {
+                    // если левая вилка занята, философ думает 18 мс
+                    think();
+                }
             }
+            // завершается поток сообщением, что философ насытился и выводится вся история о нем
+            log.append("* СЫТЫЙ");
+            System.out.println(name + ": " + log);
         } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
         }
     }
 
-    private void think() throws InterruptedException {
+    public void think()  {
         state = PhilosopherState.THINKING;
         System.out.println("Philosopher " + name + " is thinking.");
-        Thread.sleep(100); // Simulating thinking
+        log.append("* подумал ");
+        try {
+            sleep(18);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    private void eat() throws InterruptedException {
-        state = PhilosopherState.HUNGRY;
-        System.out.println("Philosopher " + name + " is hungry.");
-        leftFork.pickUp();
-        System.out.println("Philosopher " + name + " picks up left fork.");
-        rightFork.pickUp();
-        System.out.println("Philosopher " + name + " picks up right fork and eats.");
-        state = PhilosopherState.EATING;
-        eatCount++;
-        System.out.println("Philosopher " + name + " eats. (" + eatCount + " time)");
-        rightFork.putDown();
-        System.out.println("Philosopher " + name + " puts down right fork.");
-        leftFork.putDown();
-        System.out.println("Philosopher " + name + " puts down left fork.");
-    }
 
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(name);
+
+        // проверяем, какое значение атомарного поля установлено у вилки, когда философ есть ею
+        if (leftFork.getOwnerNumber().doubleValue() == number)
+            sb.append(": ").append(leftFork.toString());
+        if (rightFork.getOwnerNumber().doubleValue() == number)
+            sb.append(": ").append(rightFork.toString());
+        return sb.toString();
+    }
 }
